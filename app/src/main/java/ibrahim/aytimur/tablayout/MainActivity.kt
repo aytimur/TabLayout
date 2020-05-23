4adee8d681fe94bb69bdf6c300ed047a0f189ac4
aytimur <32987539+aytimur@users.noreply.github.com>
2020-05-23 11:25:52 +0300

    TabLayout

---
+++ app/src/main/java/ibrahim/aytimur/tablayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package ibrahim.aytimur.tablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
